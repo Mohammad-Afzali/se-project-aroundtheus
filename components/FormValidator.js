64b43f6a76f8f77e20acc1ae994f295facf114a4
Mohammad Afzali <afzali.mamoon@gmail.com>
2024-03-05 22:50:50 -0500

    update FormValidator.js

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -2,9 +2,9 @@ export default class FormValidator {
   constructor(settings, formElement) {
     this._inputSelector = settings.inputSelector;
     this.submitButtonSelector = settings.submitButtonSelector;
-    this._inactiveButtonClass = settings.inaiveButtonClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
     this.inputErrorClass = settings.inputErrorClass;
-    this.errorClass = settings.errorClass;
+    this.errorClass = settings.errorClass;t
 
     this._form = formElement;
   }
