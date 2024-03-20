d41e627cef156f943abb867a3a4d463ce1b43306
Mohammad Afzali <afzali.mamoon@gmail.com>
2024-03-19 23:48:22 -0400

    fixed Webpack error

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,13 +2,13 @@ export default class Popup {
     constructor(popupSelector) {
       this._popupElement = document.querySelector(popupSelector);
     }
-    //opens popup
+    
     open() {
       this._popupElement.classList.add("modal_opened");
       document.addEventListener("keydown", this._handleEscClose);
       document.addEventListener("mousedown", this._closeModalOnRemoteClick);
     }
-    //close popup
+   
     close() {
       this._popupElement.classList.remove("modal_opened");
       document.removeEventListener("keydown", this._handleEscClose);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import FormValidator from "./components/FormValidator.js";
-import Card from"./components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import Card from"../components/Card.js";
 import "./index.css";
-import PopupWithImage from "./components/PopupWithImage.js";
-import PopupWithForm from "./components/PopupWithForm.js";
-import Section from "./components/Section.js";
-import UserInfo from "./components/UserInfo.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js";
 import {
   initialCards,
   validationSettings,
