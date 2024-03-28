aacf8a95a38db0aba617c44417b84780b0d9f6c5
Mohammad Afzali <afzali.mamoon@gmail.com>
2024-03-28 15:43:10 -0400

    update modal image

--- src/components/ModalWithImage.js
+++ src/components/ModalWithImage.js
@@ -1,18 +1,19 @@
 import Modal from "./Modal.js";
 
 export default class ModalWithImage extends Modal {
-  constructor(modalSelector) {
+  constructor(modalSelector) {gi
     super({ modalSelector });
     this._previewImage = this._modalElement.querySelector(".modal__image");
     this._previewImageTitle = this._modalElement.querySelector(
-      ".modal__image-title"
+      ".modal__image-preview"
     );
   }
 
   open(name, link) {
     this._previewImage.src = link;
-    this._Image.alt = name;
-    this._ImageTitle.textContent = name;
+    this._previewImage.alt = name;
+    console.log(this._previewImageTitle);
+    this._previewImageTitle.textContent = name;
     super.open();
   }
 }
